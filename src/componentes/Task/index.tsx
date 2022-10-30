0b226ec2b4d9ea17ac2c8a1a2d906b26de2df505
walissonsou <walissonsouza96@hotmail.com>
2022-10-30 16:31:48 -0300

    Function delete

--- src/componentes/Task/index.tsx
+++ src/componentes/Task/index.tsx
@@ -1,24 +1,35 @@
-import { ITask } from '../../App';
+import { useContext } from 'react';
 import trash from '../../assets/trash.svg'
+import { TodoContext } from '../Context/TodoProvider';
+import { ITask } from '../Context/types';
 import styles from './task.module.css'
 
-interface Prop{
+interface Prop {
   task: ITask;
 }
 
-export default function Task({task}: Prop) {
+export default function Task({ task }: Prop) {
+
+  const { tasks, setTasks } = useContext(TodoContext)
+
+  function DeleteTaskById(taskId: any) {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    console.log('Deletado')
+    setTasks(newTasks);
+  };
+
   return (
     <div className={styles.container}>
       <button className={styles.checkContainer}>
 
         <div>
-        {task.isCompleted}
+          {task.isCompleted}
         </div>
       </button>
       <p>
         {task.title}
       </p>
-      <button className={styles.trash}>
+      <button className={styles.trash} onClick={() => DeleteTaskById(task.id)}>
         <img src={trash} />
       </button>
     </div>
